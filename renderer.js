fd584d136acca85cccab86a1127ceec754490210
AbdullhRtima <abood.art.059@gmail.com>
2018-10-27 16:26:31 +0200

    hospital v1.0.0

---
+++ renderer.js
@@ -0,0 +1,22 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+// $(document).ready(function(){
+//     $('#goRight').on('click', function(){
+//       $('#slideBox').animate({
+//         'marginLeft' : '0'
+//       });
+//       $('.topLayer').animate({
+//         'marginLeft' : '100%'
+//       });
+//     });
+//     $('#goLeft').on('click', function(){
+//       $('#slideBox').animate({
+//         'marginLeft' : '50%'
+//       });
+//       $('.topLayer').animate({
+//         'marginLeft': '0'
+//       });
+//     });
+//   });
